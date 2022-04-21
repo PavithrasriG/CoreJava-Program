6548534452dbf606c29f2627de8d3688ca7ed85f
PavithrasriG <99879090+PavithrasriG@users.noreply.github.com>
2022-04-21 11:51:32 +0530

    Add files via upload

---
+++ DeclareVariableMainClass/src/pack2/BasicDeclareVariable.java
@@ -0,0 +1,21 @@
+package pack2;
+
+public class BasicDeclareVariable {
+
+	public static void main(String[] args) {
+		int i;
+		i=10;
+		float f;
+		f=56.6f;
+		double dval;
+		dval=675.3;
+		char ch;
+		ch='A';
+		boolean b;
+		b=false;
+		String s="Ginu";
+		System.out.println("Integer Value "+1);
+
+	}
+
+}
---
+++ DeclareVariableMainClass/src/pack2/BasicPrinttheValue.java
@@ -0,0 +1,14 @@
+package pack2;
+
+public class BasicPrinttheValue {
+
+	public static void main(String[] args) {
+		System.out.print("Hello "); //after hello enter ur name
+		System.out.println("Warm Reagards");
+		System.out.println("Welcome");
+		System.out.println("to");
+		System.out.println("the course");
+
+	}
+
+}
